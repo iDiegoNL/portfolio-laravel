653a08c8112799eb48ac42307d3f33c166012a10
Diego <me@diegor.nl>
2019-12-18 18:50:59 +0100

    Initial commit

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,43 @@
+<div id="nav-menu">
+    <a class="logo">
+        <h1>DiegoR</h1>
+    </a>
+    <ul class="navigation">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="https://diegor.carbonmade.com/about">About</a></li>
+        <li><a href="https://diegor.carbonmade.com">Portfolio</a></li>
+        <li><a href="https://diegor.carbonmade.com/contact">Contact</a></li>
+    </ul> <!-- cd-single-item-wrapper -->
+
+    <div class="nav-social">
+        <ul>
+            <li>
+                <a href="https://github.com/iDiegoNL" target="_blank">
+                    <ion-icon name="logo-github"></ion-icon>
+                </a>
+            </li>
+            <li>
+                <a href="https://twitter.com/itsdiegooo_" target="_blank">
+                    <ion-icon name="logo-twitter"></ion-icon>
+                </a>
+            </li>
+            <li>
+                <a href="https://unsplash.com/@idiego" target="_blank">
+                    <ion-icon name="image"></ion-icon>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/diego-relyveld/" target="_blank">
+                    <ion-icon name="logo-linkedin"></ion-icon>
+                </a>
+            </li>
+            <li>
+                <a href="https://tweakers.net/gallery/666424/" target="_blank">
+                    <ion-icon name="construct"></ion-icon>
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <p class="copyright-text">© Copyright 2019 - DiegoR</p>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,238 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Diego Relyveld</title>
+    <meta name="description"
+          content="Hello there! My name is Diego Relyveld, I am a freelance PHP developer that lives in Amsterdam."/>
+    <meta name="robots" content="index, follow"/>
+    <meta name="referrer" content="always"/>
+
+    <meta property="og:title" content="Diego Relyveld"/>
+    <meta property="og:description"
+          content="Hello there! My name is Diego Relyveld, I am a freelance PHP developer that lives in Amsterdam."/>
+    <meta property="og:image" content="/assets/images/asoggetti-LFjxCbhs0QM-unsplash.jpg">
+    <meta property="og:url" content="https://portfolio.diegor.nl"/>
+    <meta name="twitter:title" content="Diego Relyveld">
+    <meta name="twitter:description"
+          content="Hello there! My name is Diego Relyveld, I am a freelance PHP developer that lives in Amsterdam."/>
+    <meta name="twitter:image"
+          content="https://firebasestorage.googleapis.com/v0/b/diegor-fd963.appspot.com/o/assets%2Fimages%2Fasoggetti-LFjxCbhs0QM-unsplash.jpg?alt=media&token=cffb06b8-aa6c-4a86-99b6-b7760f4e7a1e"/>
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:site" content="@ItsDiegooo_"/>
+    <meta name="twitter:creator" content="@ItsDiegooo_"/>
+
+    <link href="https://fonts.googleapis.com/css?family=Saira:300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,400i,500,500i,600,600i,700" rel="stylesheet">
+    <link href="{{ asset('css/plugins.css') }}" rel="stylesheet" type="text/css" media="all"/>
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" media="all"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous" />
+</head>
+<body>
+<div class="beker_tm_all_wrap">
+    <div class="header_wrap">
+        <div class="logo anchor">
+            <div class="logo_image">
+                <a href="#home"><img src="img/logo/logo.png" alt="" /></a>
+            </div>
+        </div>
+        <div class="trigger">
+            <a href="#">
+                <span class="one"></span>
+                <span class="two"></span>
+            </a>
+        </div>
+    </div>
+    <div class="beker_tm_menu">
+        <div class="nav_list">
+            <ul class="anchor_nav">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <!-- HERO -->
+    <div class="beker_tm_section" id="home">
+        <div class="beker_tm_hero_wrap" data-img-url="https://firebasestorage.googleapis.com/v0/b/diegor-fd963.appspot.com/o/assets%2Fimages%2Fasoggetti-LFjxCbhs0QM-unsplash.jpg?alt=media&token=cffb06b8-aa6c-4a86-99b6-b7760f4e7a1e">
+            <div class="overlay"></div>
+            <div class="texts_wrap">
+                <div class="name_holder">
+                    <h3>
+                        Hi, I'm Diego
+                    </h3>
+                </div>
+                <div class="job_holder">
+                    <p>
+                        I'm a developer living and learning in Amsterdam.
+                    </p>
+                </div>
+            </div>
+            <div class="beker_tm_down anchor">
+                <div class="down-icon white top_120">
+                    <a href="#about">
+                        <svg width="26px" height="100%" viewBox="0 0 247 390" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;">
+                            <path id="wheel" d="M123.359,79.775l0,72.843" style="fill:none;stroke:#fff;stroke-width:20px;"/>
+                            <path id="mouse" d="M236.717,123.359c0,-62.565 -50.794,-113.359 -113.358,-113.359c-62.565,0 -113.359,50.794 -113.359,113.359l0,143.237c0,62.565 50.794,113.359 113.359,113.359c62.564,0 113.358,-50.794 113.358,-113.359l0,-143.237Z" style="fill:none;stroke:#fff;stroke-width:20px;"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /HERO -->
+
+    <!-- ABOUT -->
+    <div class="beker_tm_section" id="about">
+        <div class="beker_tm_main_content">
+            <div class="beker_tm_about_wrap">
+                <div class="container">
+                    <div class="author_image">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/diegor-fd963.appspot.com/o/assets%2Fimages%2Favatar.jpg?alt=media&token=9313d0f2-addd-4cc5-ad34-a5d774104202" alt="" />
+                    </div>
+                    <div class="author_info">
+                        <p>
+                            Hi! My name is <strong>Diego Relyveld</strong>. I am a freelance back-end developer and system administrator that currently lives in Amsterdam. In my free time, I love programming with languages such as PHP, and also traveling around the world.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /ABOUT -->
+
+    <!-- SERVICES -->
+    <div class="beker_tm_section" id="services">
+        <div class="container">
+            <div class="beker_tm_main_title">
+                <span>Awesome services</span>
+                <h3>I recommend you high quality services</h3>
+            </div>
+            <div class="beker_tm_services beker_tm_waypoint_effect">
+                <ul>
+                    <li>
+                        <div class="inner">
+                            <img class="svg" src="https://firebasestorage.googleapis.com/v0/b/diegor-fd963.appspot.com/o/assets%2Fimages%2Flaravel.svg?alt=media&token=12b0bf6f-d9e7-4130-a02a-c0cedf98dd0b" alt="" />
+                            <h3>Laravel</h3>
+                            <p>Lorem Ipsum</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- /SERVICES -->
+
+    <!-- PROJECTS -->
+    <div class="beker_tm_section" id="portfolio">
+        <div class="container">
+            <div class="beker_tm_main_title">
+                <span>Creative Projects</span>
+                <h3>Check out my latest projects</h3>
+            </div>
+            <div class="beker_tm_projects_wrap beker_tm_waypoint_effect">
+                <ul class="beker_tm_portfolio_list">
+                    <li>
+                        <div class="list_inner">
+                            <div class="image_wrap">
+                                <img src="placeholder.png" alt="" />
+                                <div class="main_image" data-img-url="placeholder.png"></div>
+                            </div>
+                            <div class="overlay"></div>
+                            <div class="overlay_text">
+                                <h3>Placeholder</h3>
+                                <span>Placeholder</span>
+                            </div>
+                            <a class="full_link" href="placeholder"></a>
+                        </div>
+                    </li>
+                </ul>
+                <div class="beker_tm_load_more">
+                    <div class="inner">
+                        <a href="#"><span>View all</span><img class="svg" src="img/svg/spinner.svg" alt="" /></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /PROJECTS -->
+
+    <!-- CONTACT -->
+    <div class="beker_tm_section" id="contact">
+        <div class="container">
+            <div class="beker_tm_main_title contact ">
+                <span>Contact me</span>
+                <h3>Get in touch with me</h3>
+            </div>
+            <div class="beker_tm_contact_wrap beker_tm_waypoint_effect">
+                <div class="leftpart">
+                    <ul>
+                        <li>
+                            <div class="inner">
+                                <img class="svg" src="img/svg/placeholder.svg" alt="" />
+                                <span>Placeholder</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="inner">
+                                <img class="svg" src="img/svg/telephone.svg" alt="" />
+                                <span>Placeholder</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="inner">
+                                <img class="svg" src="img/svg/envelope.svg" alt="" />
+                                <span><a href="#">Placeholder</a></span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="rightpart">
+                    <div class="contact_list_wrap">
+                        <form action="/" method="post" class="contact_form" id="contact_form">
+                            <div class="returnmessage" data-success="Your message has been received, We will contact you soon."></div>
+                            <div class="empty_notice"><span>Please Fill Required Fields</span></div>
+                            <div class="first_row">
+                                <div class="first">
+                                    <input id="name" type="text" placeholder="Your Name" />
+                                </div>
+                                <div class="second">
+                                    <input id="email" type="text" placeholder="Your Email" />
+                                </div>
+                            </div>
+                            <div class="second_row">
+                                <textarea id="message" placeholder="Your Message"></textarea>
+                            </div>
+                            <div class="beker_tm_button" data-position="center">
+                                <a id="send_message" href="#"><span>Submit</span></a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /CONTACT -->
+    <!-- COPYRIGHT -->
+    <div class="beker_tm_section">
+        <div class="beker_tm_copyright_wrap">
+            <div class="container">
+                <div class="inner_wrap">
+                    <p class="copyright">Copyright &copy; 2019. All Rights are Reserved.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /COPYRIGHT -->
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="{{ asset('js/plugins.js') }}"></script>
+<script src="{{ asset('js/init.js') }}"></script>
+</body>
+</html>
